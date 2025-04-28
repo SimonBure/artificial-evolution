92500ba419fb0640f32c29e25dfed53ad7b2c5b6
simon <simon.bure@mailo.com>
2025-04-28 15:32:55 +0200

    Genome fitness computation

--- rules.py
+++ rules.py
@@ -8,3 +8,26 @@ SEXUAL_REPRODUCTION_RATE = 0.5
 SELF_REPRODUCTION_RATE = 0.5
 
 ALPHABET = ('0', '1')
+
+def compute_sequence_fitness(sequence: str) -> int:
+    fitness = 0
+    stored_1 = 0
+    ones_in_a_row = 0
+    stored_0 = 0
+    zeros_in_a_row = 0
+    
+    for char in sequence:
+        if char == '1':
+            fitness += 1
+            
+            stored_1 += 1
+            ones_in_a_row += 1
+            zeros_in_a_row = 0
+        else:
+            
+            
+            stored_0 += 1
+            zeros_in_a_row += 1
+            ones_in_a_row = 0
+            
+    return fitness
